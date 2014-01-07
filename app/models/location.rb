82c932a3ae8a5a64c68fc9b643128c3bddf992d0
Csaba Nagy <Csaba.Nagy02@t-systems.com>
2014-01-08 00:55:52 +0100

    Adding callback to model

--- app/models/location.rb
+++ app/models/location.rb
@@ -7,5 +7,11 @@ class Location
 
   index({from_position: '2dsphere'}, background: true)
   index({to_position: '2dsphere'}, background: true)
+  after_validation :transform_from_position
+  
+  private
 
+  def transform_from_position
+    self.from_position = [longitude.to_f, latitude.to_f ]
+  end
 end
